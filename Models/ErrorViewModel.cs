b3bf79e44016c924bb18a33426b287cf289a2dc8
vesper0990 <kamilsoloducha@gmail.com>
2018-07-07 08:57:12 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace vesper_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
