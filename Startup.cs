9f8e4c83105140365954fb4e8b9a88b114fb777b
DS\KFG864 <kamil.soloducha@motorolasolutions.com>
2018-07-12 07:53:54 +0200

    add database connection

--- Startup.cs
+++ Startup.cs
@@ -2,18 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
 
 namespace vesper_test
 {
     public class Startup
     {
+		
+		private readonly string _connectionString;
+		
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+			_connectionString = @"Server=db; Database=Students; Uid=user_name_1; Pwd=my-secret-pw";
         }
 
         public IConfiguration Configuration { get; }
@@ -21,6 +28,7 @@ namespace vesper_test
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+			WaitForDBInit(_connectionString);
             services.AddMvc();
         }
 
@@ -44,6 +52,27 @@ namespace vesper_test
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+		
+		private static void WaitForDBInit(string connectionString)
+        {
+            var connection = new MySqlConnection(connectionString);
+            int retries = 1;
+            while (retries < 7)
+            {
+                try
+                {
+                    Console.WriteLine("Connecting to db. Trial: {0}", retries);
+                    connection.Open();
+                    connection.Close();
+                    break;
+                }
+                catch (MySqlException)
+                {
+                    Thread.Sleep((int) Math.Pow(2, retries) * 1000);
+                    retries++;
+                }
+            }
         }
     }
 }
